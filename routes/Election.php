59532aeb2b12e4972b7fee76ed420251e4e792b2
hussainmemon2 <97228832+hussainmemon2@users.noreply.github.com>
2026-01-23 15:43:03 +0500

    p

--- routes/Election.php
+++ routes/Election.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Users\ElectionApplicationController;
 use App\Http\Controllers\Api\Users\ElectionController;
 use App\Http\Controllers\Api\Users\ElectionPaymentController;
+use App\Models\ElectionVote;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('api.auth')->prefix('elections')->group(function () {
@@ -17,5 +18,8 @@ Route::middleware('api.auth')->prefix('elections')->group(function () {
     Route::controller(ElectionPaymentController::class)->group(function () {
         Route::post('/{election}/pay-application-fee', 'payApplicationFee');
         Route::post('/{election}/pay-submission-fee',  'paySubmissionFee');
+    });
+    Route::controller(ElectionVote::class)->group(function () {
+        Route::get("{election}/candidates", 'listCandidates');
     });
 });
